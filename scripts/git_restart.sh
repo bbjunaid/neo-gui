31089af863beec2a2238be57772e62eb15918f79
user <doublelifttechnologies@gmail.com>
2017-10-06 17:23:07 -0700

    updating

---
+++ scripts/git_restart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm scripts/commands/restart_sync
+git commit -am 'SERVER RESTART OK'
+git push origin master
--- scripts/git_test.sh
+++ scripts/git_test.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-rm scripts/commands/restart_sync
-git commit -am 'SERVER RESTART OK'
-git push origin master
+git ls-remote git@github.com:bbjunaid/neo-gui.git
