27c9ed1b9ac0b240080ae426bcfd72cb6eabcde4
user <doublelifttechnologies@gmail.com>
2017-10-08 02:18:50 -0700

    fix

--- bootstrap_mainnet_chain.sh
+++ bootstrap_mainnet_chain.sh
@@ -5,6 +5,9 @@ echo "Getting MainNet Chain"
 wget 'https://www.dropbox.com/s/jvhgetnlq8653ka/Chain.zip?dl=1'
 echo 'Unzipping..'
 unzip Chain.zip\?dl=1
-mkdir ./neo-gui/bin/Release/Chain
+#mkdir ./neo-gui/bin/Release/Chain
 mv Chain ./neo-gui/bin/Release/
 echo 'Cleaning up'
+cd ./neo-gui/bin/Release
+mkdir Chain
+mv * Chain
