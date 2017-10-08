b520543611d348cd1ed071a31d349dc2a2ba145b
user <doublelifttechnologies@gmail.com>
2017-10-07 23:47:32 -0700

    updating chain scripts

--- bootstrap_testnet_chain.sh
+++ bootstrap_testnet_chain.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Getting Testnet Chain"
+rm -rf ./neo-gui/gui_test/Debug/ChainTestNet
 wget 'https://www.dropbox.com/sh/gbrggfzojqjdltf/AABzUkaacmhyTRSwsMxFEMCla/ChainTestNet.zip?dl=1'
 echo 'Unzipping..'
 unzip ChainTestNet.zip\?dl=1
