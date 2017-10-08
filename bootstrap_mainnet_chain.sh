ce6463bf6b438b9e71cc93958f5f7bb4041d8465
user <doublelifttechnologies@gmail.com>
2017-10-08 01:57:36 -0700

    changing perms

--- bootstrap_mainnet_chain.sh
+++ bootstrap_mainnet_chain.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Getting MainNet Chain"
-wget 'https://www.dropbox.com/s/wpg03j503y3hucq/Chain.zip?dl=1'
+#wget 'https://www.dropbox.com/s/wpg03j503y3hucq/Chain.zip?dl=1'
+wget ''
 echo 'Unzipping..'
 unzip Chain.zip\?dl=1
 mkdir ./neo-gui/bin/Release/Chain
---
+++ perms.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+chmod +x neo-gui/bin/Release/neo-gui.exe
+chmod +x neo-gui/bin/Debug/neo-gui.exe
+chmod +x neo-gui/bin/Debug/x64/sqlite3.dll
+chmod +x neo-gui/bin/Release/x64/sqlite3.dll
+chmod +x neo-gui/bin/Debug/libleveldb.dll
+chmod +x neo-gui/bin/Release/libleveldb.dll
