e2fbabe38fa2c49cd5ba847198b64d7c825a4ff6
user <doublelifttechnologies@gmail.com>
2017-10-08 02:01:14 -0700

    updating chain scripts

--- bootstrap_mainnet_chain.sh
+++ bootstrap_mainnet_chain.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Getting MainNet Chain"
 #wget 'https://www.dropbox.com/s/wpg03j503y3hucq/Chain.zip?dl=1'
-wget ''
+wget 'https://drive.google.com/uc?export=download&confirm=1uIa&id=0Bwcc-DUGt9pjTnhVdW1QbHl4Wkk'
 echo 'Unzipping..'
 unzip Chain.zip\?dl=1
 mkdir ./neo-gui/bin/Release/Chain
